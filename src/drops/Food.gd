180845dfb937c366ace04a7ea235767d1c15c311
Corey Paskett <paskett@gmail.com>
2020-04-18 12:38:55 -0600

    Added some falling food with some collision detection for floor/player

---
+++ src/drops/Food.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+const GRAVITY = 50
+var velocity = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity.y += GRAVITY * delta
+	position.y += velocity.y * delta
+
+
+func _on_Food_body_entered(body):
+	print(body.get_name())
+	if body.get_name() == "Player":
+		print("scored")
+	if body.get_name() == "Floor":
+		print("hit floor")
+	
+	queue_free()
