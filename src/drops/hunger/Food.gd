65ccd0c1a5b2a7394597a681762bbc28f2bf0fe9
Corey Paskett <paskett@gmail.com>
2020-04-18 22:08:22 -0600

    refactored level stuff a bit

--- src/drops/hunger/Food.gd
+++ src/drops/hunger/Food.gd
@@ -1,9 +1,5 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 const GRAVITY = 50
 var velocity = Vector2()
 signal caught_food
--- src/game/levels/hunger/Hunger.gd
+++ src/game/levels/hunger/Hunger.gd
@@ -4,12 +4,15 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var pizza_scene = load("res://src/drops/Pizza.tscn")
-onready var cherry_scene = load("res://src/drops/Cherry.tscn")
-onready var banana_scene = load("res://src/drops/Banana.tscn")
+onready var pizza_scene = load("res://src/drops/hunger/Pizza.tscn")
+onready var cherry_scene = load("res://src/drops/hunger/Cherry.tscn")
+onready var banana_scene = load("res://src/drops/hunger/Banana.tscn")
 onready var random = RandomNumberGenerator.new()
 onready var screen_size = get_viewport().get_visible_rect().size
-onready var hunger_meter = $"Overlay/Control/HungerMeter"
+onready var hunger_meter = $Overlay/Control/HungerMeter
+var food
+
+signal level_end
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,11 +21,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if !has_node("Pizza") and !has_node("Cherry") and !has_node("Banana"):
+	var hunger_value = hunger_meter.get_value()
+	if hunger_value == 100 or hunger_value == 0:
+		emit_signal("level_end")
+	elif !has_node("Pizza") and !has_node("Cherry") and !has_node("Banana"):
 		random.randomize()
 		var spawn_choice = random.randi()%3
-		
-		var food
 		
 		if spawn_choice == 0:
 			food = pizza_scene.instance()
